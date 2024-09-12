27e1ab8f024c4af87c45826444af4d0b6796dd0f
Douglas Soares <douglassoares.cinema@gmail.com>
2024-09-12 03:30:49 -0300

    Primeira Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Buscador de Eventos
+## Desenvolvido no curso técnico de Informática para a Internet na ETEC de Praia Grande
+
+&nbsp;
+
+O site foi desenvolvido com base em uma atividade para a matéria de Interfaces Web II. Nele, foram utilizadas as tecnologias HTML, CSS e Javascript. O objetivo principal do projeto era o treino do uso de switch case, além da estilização baseada em um template e da responsividade para smartphones.
+O Buscador de Eventos retorna uma sugestão com base no seu interesse (alto, médio ou baixo) por 4 tipos diferentes de eventos: Música, Teatro, Esporte e Comédia.
+&nbsp;
---
+++ scripts/script.js
@@ -0,0 +1,91 @@
+function envia(){
+    const tipoDeEvento = document.getElementById("tipoDeEvento").value.toLowerCase();
+    const nivelDeInteresse = document.getElementById("nivelDeInteresse").value.toLowerCase();
+    const resultado = document.getElementById("resultado");
+
+    switch(tipoDeEvento){
+        case "musica":
+           switch(nivelDeInteresse){
+                case "alto":
+                    resultado.innerHTML = "Confira o concerto da sua banda favorita! É o evento imperdível do mês!";
+                break;
+
+                case "medio":
+                    resultado.innerHTML = "Não perca o festival de música local. Será uma ótima experiência!";
+                break;
+
+                case "baixo":
+                    resultado.innerHTML = "Talvez você curta um show em um bar local. É uma boa maneira de passar a noite.";
+                break;
+
+                default:
+                    resultado.innerHTML = "Para expressar seu nível de interesse, use apenas os termos \"alto\", \"medio\" ou \"baixo\", sem acentuações.";
+                break;
+           }
+        break;
+
+        case "teatro":
+            switch(nivelDeInteresse){
+                case "alto":
+                    resultado.innerHTML = "Assista à peça aclamada pela crítica no teatro principal da cidade!";
+                break;
+
+                case "medio":
+                    resultado.innerHTML = "Dê uma olhada nas produções teatrais locais. Há algumas boas opções!";
+                break;
+
+                case "baixo":
+                    resultado.innerHTML = "TUma peça amadora no centro comunitário pode ser um passatempo interessante.";
+                break;
+
+                default:
+                    resultado.innerHTML = "Para expressar seu nível de interesse, use apenas os termos \"alto\", \"medio\" ou \"baixo\", sem acentuações.";
+                break;
+           }
+        break;
+
+        case "esporte":
+            switch(nivelDeInteresse){
+                case "alto":
+                    resultado.innerHTML = "Não perca o jogo do campeonato. A atmosfera será incrível!";
+                break;
+
+                case "medio":
+                    resultado.innerHTML = "Confira o jogo da liga local. É uma boa maneira de se envolver com a comunidade.";
+                break;
+
+                case "baixo":
+                    resultado.innerHTML = "Talvez um jogo de exibição em um estádio menor possa ser uma opção.";
+                break;
+
+                default:
+                    resultado.innerHTML = "Para expressar seu nível de interesse, use apenas os termos \"alto\", \"medio\" ou \"baixo\", sem acentuações.";
+                break;
+           }
+        break;
+
+        case "comedia":
+            switch(nivelDeInteresse){
+                case "alto":
+                    resultado.innerHTML = "Garanta seus ingressos para o show de stand-up do comediante famoso!";
+                break;
+
+                case "medio":
+                    resultado.innerHTML = "Veja as noites de comédia no clube local. Pode ser divertido!";
+                break;
+
+                case "baixo":
+                    resultado.innerHTML = "Um karaokê em um café pode ser uma maneira leve e divertida de passar o tempo.";
+                break;
+
+                default:
+                    resultado.innerHTML = "Para expressar seu nível de interesse, use apenas os termos \"alto\", \"medio\" ou \"baixo\", sem acentuações.";
+                break;
+           }
+        break;
+
+        default:
+            resultado.innerHTML= `O evento "${tipoDeEvento}" não foi encontrado. Tente digitar sem usar acentuações.`;
+        break;
+    }
+}
